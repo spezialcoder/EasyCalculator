16a58ed40812ef4f4951b188ab835731c107f40b
Lewin Sorg <developermind405@gmail.com>
2020-10-14 18:35:11 +0200

    Add files via upload

--- Calc/calculator.js
+++ Calc/calculator.js
@@ -11,9 +11,12 @@ var eq = document.getElementById("eq");
 var plus = document.getElementById("plus");
 var minus = document.getElementById("minus");
 var output = document.getElementById("output");
+var divide = document.getElementById("divide");
+var zero = document.getElementById("0");
+var multi = document.getElementById("multi");
 
 now = "";
-
+var charse = ["1","2","3","4","5","6","7","8","9","+","-","/","*"];
 function interpretInp(num) {
     if(now.length <= 15) {
         now += num;
@@ -44,4 +47,19 @@ n8.onclick = function() {interpretInp("8")};
 n9.onclick = function() {interpretInp("9")};
 plus.onclick = function() {interpretInp("+")};
 minus.onclick = function() {interpretInp("-")};
+divide.onclick = function() {interpretInp("/")};
+multi.onclick = function() {interpretInp("*")};
+zero.onclick = function() {interpretInp("0")};
 eq.onclick = evaluate;
+
+document.addEventListener("keydown",function(event) {
+    console.log(event.key);
+     if(event.key == "Backspace") {
+         now = "";
+         output.innerHTML = now;
+     } else if(event.key == "Enter") {
+         evaluate();
+     } else if(charse.includes(event.key.trim())) {
+         interpretInp(event.key);
+     }
+})
