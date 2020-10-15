334ff36d5170a1bd6f9f06f155e34d4301398e64
Lewin Sorg <developermind405@gmail.com>
2020-10-15 17:29:48 +0200

    Update calculator.js

--- Calc/calculator.js
+++ Calc/calculator.js
@@ -17,7 +17,7 @@ var multi = document.getElementById("multi");
 var clean = document.getElementById("clean");
 
 now = "";
-var charse = ["1","2","3","4","5","6","7","8","9","+","-","/","*"];
+var charse = ["0","1","2","3","4","5","6","7","8","9","+","-","/","*"];
 
 function interpretInp(num) {
     now += num;
